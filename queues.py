08bf793cbacda266ba3dbddcdca200b0161df140
Lee Anne <leeangeles2@gmail.com>
2022-12-13 14:56:11 +0800

    Enqueue Implemented

--- queues.py
+++ queues.py
@@ -6,3 +6,8 @@ from collections import deque
 class Queue:
     def __init__(self):
         self._elements = deque()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    
