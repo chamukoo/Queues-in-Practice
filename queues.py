17d940860727c8e7e5a967513b77751974e380e2
Lee Anne <leeangeles2@gmail.com>
2022-12-13 15:59:12 +0800

    Priority: Initializing Class

--- queues.py
+++ queues.py
@@ -1,6 +1,7 @@
 # Queues: Firs-In, First-Out (FIFO)
 
 from collections import deque
+from heapq import heappop, heappush
 
 # Initializing Class: Queue
 class Queue:
@@ -25,4 +26,16 @@ class Queue:
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
+
+
+# Initializing Class: Priority Queue
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)
         
