27a1361816b53643b2bc252e3889ff9ec7f9bcb9
Lee Anne <leeangeles2@gmail.com>
2022-12-13 16:34:36 +0800

    Final Commit in queues

--- queues.py
+++ queues.py
@@ -4,8 +4,18 @@ from collections import deque
 from heapq import heappop, heappush
 from itertools import count
 
+# Initializing Class: Interable Misin
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+
 # Initializing Class: Queue
-class Queue:
+class Queue(IterableMixin):
     def __init__(self, *elements):
         self._elements = deque(elements)
 
@@ -30,44 +40,22 @@ class Stack(Queue):
 
 
 # Initializing Class: Priority Queue
-class PriorityQueue:
+class PriorityQueue(IterableMixin):
     def __init__(self):
         self._elements = []
+        self._counter = count()
 
     def enqueue_with_priority(self, priority, value):
         # heappush(self._elements, (priority, value))
 
         # Making the priority a negative number so that 
         # the highest one becomes the lowest
-        heappush(self._elements, (-priority, value))
+        # heappush(self._elements, (-priority, value))
 
-
-    def dequeue(self):
-        # return heappop(self._elements)
-        return heappop(self._elements)[1]
-
-
-# Initializing Class: Priority
-class PriorityQueue:
-    def __init__(self):
-        self._elements = []
-        self._counter = count()
-
-    def enqueue_with_priority(self, priority, value):
         element = (-priority, next(self._counter), value)
         heappush(self._elements, element)
 
     def dequeue(self):
+        # return heappop(self._elements)
+        # return heappop(self._elements)[1]
         return heappop(self._elements)[-1]
-
-
-# Initializing Class: Interable Misin
-class IterableMixin:
-    def __len__(self):
-        return len(self._elements)
-
-    def __iter__(self):
-        while len(self) > 0:
-            yield self.dequeue()
-
-            
