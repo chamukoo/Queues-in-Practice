2c5d16ab49a78f355bbaf19a7f86a8c2a0ad9aea
Lee Anne <leeangeles2@gmail.com>
2022-12-13 15:49:26 +0800

    Heap: Pop Elements

---
+++ heap.py
@@ -0,0 +1,17 @@
+from heapq import heappush
+
+# List
+fruits = []
+
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+# Displaying fruits
+print(fruits)
+
+from heapq import heappop
+
+# Popping elements
+print(heappop(fruits))
+
