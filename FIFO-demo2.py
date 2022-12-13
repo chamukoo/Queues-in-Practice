8df06508b7ffece07f3cfe3f9dacf30ff4831d53
Lee Anne <leeangeles2@gmail.com>
2022-12-13 15:34:04 +0800

    FIFO: DIsplay the length after for loop

--- FIFO-demo2.py
+++ FIFO-demo2.py
@@ -8,3 +8,7 @@ print(len(fifo))
 # Displaying the Elements
 for element in fifo:
     print(element)
+
+# The queue has 3 elements initially, but its length
+# drops to 0 after consuming all elements in a loop.
+print(len(fifo))
