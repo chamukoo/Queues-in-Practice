6827fa3c231d891a5cbe8c0cdaea7b8cd37ae100
Lee Anne <leeangeles2@gmail.com>
2022-12-16 01:43:08 +0800

    Queue Types Implemented

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -1,4 +1,13 @@
 # thread_safe_queues.py
 
+import argparse
+from queue import LifoQueue, PriorityQueue, Queue
+
+QUEUE_TYPES = {
+    "fifo": Queue,
+    "lifo": LifoQueue,
+    "heap": PriorityQueue
+}
+
 
 
