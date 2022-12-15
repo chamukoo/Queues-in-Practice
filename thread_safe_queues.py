cc5adcd861c181f67aa90f17bc233059e93b98d5
Lee Anne <leeangeles2@gmail.com>
2022-12-16 02:07:25 +0800

    TSQ: Class Consumer Initialized

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -95,4 +95,15 @@ class Producer(Worker):
             self.simulate_work()
             self.buffer.put(self.product)
             self.simulate_idle()
+
+
+# Initializing Class: Consumer
+class Consumer(Worker):
+    def run(self):
+        while True:
+            self.product = self.buffer.get()
+            self.simulate_work()
+            self.buffer.task_done()
+            self.simulate_idle()
             
+                   
