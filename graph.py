2a2ee628404e5eb0477590d8c45866c69afddbab
Lee Anne <leeangeles2@gmail.com>
2022-12-15 19:18:56 +0800

    Initialized Class: City

---
+++ graph.py
@@ -0,0 +1,11 @@
+from typing import NamedTuple
+
+# Initializing Class: City
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+
+    
