e601254e45b0ac4836350f2fa194c089b7ea39da
Lee Anne <leeangeles2@gmail.com>
2022-12-16 01:51:04 +0800

    TSQ: Products List Implemented

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -27,6 +27,25 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         pass
 
-    
+# List
+PRODUCTS = (
+    ":balloon:",
+    ":cookie:",
+    ":crystal_ball:",
+    ":diving_mask:",
+    ":flashlight:",
+    ":gem:",
+    ":gift:",
+    ":kite:",
+    ":party_popper:",
+    ":postal_horn:",
+    ":ribbon:",
+    ":rocket:",
+    ":teddy_bear:",
+    ":thread:",
+    ":yo-yo:",
+)
+
+
 
 
