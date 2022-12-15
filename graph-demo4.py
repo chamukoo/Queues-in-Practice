88f9f6cf3cf13e4260ce279219407b9cfae9cbcf
Lee Anne <leeangeles2@gmail.com>
2022-12-15 20:14:29 +0800

    Graph: Display the shortest path in city 1 & 2

--- graph-demo4.py
+++ graph-demo4.py
@@ -8,3 +8,9 @@ city1 = nodes["aberdeen"]
 
 # Node 2 is set in Perth
 city2 = nodes["perth"]
+
+# Display the shortest path in city1 and city2
+for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
+    print(f"{i}.", " → ".join(city.name for city in path))
+
+
