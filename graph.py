3e13d2232e64e44b68f68c480cd51fc293c071fe
Lee Anne <leeangeles2@gmail.com>
2022-12-15 23:32:06 +0800

    Def dijkstra shortest path Implemented

--- graph.py
+++ graph.py
@@ -3,6 +3,8 @@ import networkx as nx
 from queues import Queue
 from collections import deque
 from queues import Queue, Stack
+from math import inf as infinity
+from queues import MutableMinHeap, Queue, Stack
 
 # Initializing Class: City
 class City(NamedTuple):
@@ -139,3 +141,29 @@ class City(NamedTuple):
             if predicate(node):
                 return node
 
+
+    # Dijkstra’s Algorithm Using a Priority Queue
+    def dijkstra_shortest_path(graph, source, destination, weight_factory):
+        previous = {}
+        visited = set()
+        unvisited = MutableMinHeap()
+
+        for node in graph.nodes:
+            unvisited[node] = infinity
+        unvisited[source] = 0
+
+        while unvisited:
+            visited.add(node := unvisited.dequeue())
+
+            for neighbor, weights in graph[node].items():
+                if neighbor not in visited:
+                    weight = weight_factory(weights)
+                    new_distance = unvisited[node]
+
+                    if new_distance < unvisited[neighbor]:
+                        unvisited[neighbor] = new_distance
+                        previous[neighbor] = node
+                        
+        return retrace(previous, source, destination)
+
+    
