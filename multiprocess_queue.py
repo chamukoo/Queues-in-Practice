ac357c560196dfc87d4c94574899080f0d6c1390
Lee Anne <leeangeles2@gmail.com>
2022-12-16 03:26:42 +0800

    MPQ: Def Reversing MD5 Hash Implemented

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -1,2 +1,16 @@
 # multiprocess_queue.py
 
+import time
+from hashlib import md5
+from itertools import product
+from string import ascii_lowercase
+
+# Reversing an MD5 Hash on a Single Thread
+def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
+    for length in range(1, max_length + 1):
+        for combination in product(alphabet, repeat=length):
+            text_bytes = "".join(combination).encode("utf-8")
+            hashed = md5(text_bytes).hexdigest()
+            if hashed == hash_value:
+                return text_bytes.decode("utf-8")
+
