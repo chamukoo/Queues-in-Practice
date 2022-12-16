a369486425a76d542dafa57a33acf99d65ff32d3
Lee Anne <leeangeles2@gmail.com>
2022-12-16 15:55:48 +0800

    TSQ: Def Render Implemented

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -123,7 +123,22 @@ class View:
             while True:
                 live.update(self.render())
 
-    
+    def render(self):
+
+        match self.buffer:
+            case PriorityQueue():
+                title = "Priority Queue"
+                products = map(str, reversed(list(self.buffer.queue)))
+            case LifoQueue():
+                title = "Stack"
+                products = list(self.buffer.queue) 
+            case Queue():
+                title = "Queue"
+                products = reversed(list(self.buffer.queue)) 
+            case _:
+                title = products = "" 
+                
+        
 
 def main(args):
     buffer = QUEUE_TYPES[args.queue]()
