8b824a8846150ffa18381f4a0e1102ea684e458b
Lee Anne <leeangeles2@gmail.com>
2022-12-16 17:20:16 +0800

    Final Commit

--- graph-demo2.py
+++ graph-demo2.py
@@ -3,6 +3,8 @@
 import networkx as nx
 from graph import City, load_graph
 
+print("\n" + ("="*60) + "\n\n\tBreadth-First Search Using a FIFO Queue\n\n" + ("="*60))
+
 def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
 
@@ -12,14 +14,24 @@ def order(neighbors):
     return iter(sorted(neighbors, key=by_latitude, reverse=True))
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
-# for node in nx.bfs_tree(graph, nodes["edinburgh"]):
-for node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=order):
-    print("📍", node.name)
 
+# ---------- DEMO 1
+print("\n\n\tDEMO 1\n" + ("-"*60) + "\n")
+for node in nx.bfs_tree(graph, nodes["edinburgh"]):
+    print("\t📍", node.name)
     if is_twentieth_century(node.year):
-        print("Found:", node.name, node.year)
+        print("\n\tFound:", node.name, node.year)
         break
-
 else:
-    print("Not Found")
+    print("\nNot Found")
 
+# ---------- DEMO 2
+print("\n\n\tDEMO 2\n" + ("-"*60) + "\n")
+for node in nx.bfs_tree(graph, nodes["edinburgh"], sort_neighbors=order):
+    print("\t📍", node.name)
+    if is_twentieth_century(node.year):
+        print("\n\tFound:", node.name, node.year)
+        break
+else:
+    print("\nNot Found")
+print("\n" + ("="*60))
