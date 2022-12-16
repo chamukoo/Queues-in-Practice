6dfd9a7c0a1b2525272da21c962e512b6ca07a58
Lee Anne <leeangeles2@gmail.com>
2022-12-16 17:54:49 +0800

    Final Commit

--- graph-demo6.py
+++ graph-demo6.py
@@ -1,7 +1,10 @@
 # Dijkstra’s Algorithm Using a Priority Queue
 
+print("\n" + ("="*60) + "\n\n\tDijkstra’s Algorithm Using a Priority Queue\n\n" + ("="*60))
+
+
 import networkx as nx
-from graph import City, load_graph, ddijkstra_shortest_path
+from graph import City, load_graph, dijkstra_shortest_path
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
@@ -9,15 +12,23 @@ nodes, graph = load_graph("roadmap.dot", City.from_dict)
 city1 = nodes["london"]
 city2 = nodes["edinburgh"]
 
+
+# --------- DEMO 1
 def distance(weights):
     return float(weights["distance"])
 
-for city in ddijkstra_shortest_path(graph, city1, city2,distance):
-    print(city.name)
+print("\n\n\tDEMO 1 - Shortest Path by Distance\n" + ("-"*60) + "\n")
+for city in dijkstra_shortest_path(graph, city1, city2,distance):
+    print("\t" + city.name)
 
+
+# --------- DEMO 2
 def weight(node1, node2, weights):
     return distance(weights)
 
+print("\n\n\n\tDEMO 2 - Shortest Path by Weight\n" + ("-"*60) + "\n")
 for city in nx.dijkstra_path(graph, city1, city2, weight):
-    print(city.name)
-    
+    print("\t" + city.name)
+
+print("\n" + ("="*60))
+ 
